515a6cc20a45cda1aa916e9c96a60c087cd2ff75
tungtung-sketch <tungtungtutungtung@gmail.com>
2025-08-26 20:13:10 +0700

    push

--- src/i18n/I18nProvider.tsx
+++ src/i18n/I18nProvider.tsx
@@ -22,7 +22,6 @@ const translations: Record<Lang, Translations> = {
 
     // Header nav
     "nav.home": "Home",
-    "nav.aboutus" : "About us"
     "nav.news": "News",
     "nav.insights": "Insights",
     "nav.tips": "Business Tips",
@@ -226,7 +225,6 @@ const translations: Record<Lang, Translations> = {
 
     // Header nav
     "nav.home": "ホーム",
-    "nav.aboutus" : "Harmonyについて"
     "nav.news": "ニュース",
     "nav.insights": "インサイト",
     "nav.tips": "ビジネスTips",
